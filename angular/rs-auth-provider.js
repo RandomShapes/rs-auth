871f2942c3b7065e5ece3036ccdefd3bbd23f0db
Jamie Spittal <spittal@kerplunc.com>
2014-08-19 13:47:34 -0700

    Auth and Session Provider

--- angular/rs-auth-provider.js
+++ angular/rs-auth-provider.js
@@ -1,4 +1,3 @@
-var app = angular.module('rs-auth', []);
 var app = angular.module('rs-auth');
 
 /**
@@ -103,43 +102,3 @@ app.provider('$rsAuth', function () {
     };
   };
 });
-var app = angular.module('rs-auth');
-
-/**
-* Session Service
-* author: Jamie Spittal james@randomshapes.ca
-* Methods for getting and setting current session variables. Also, used for recalling a remembered session.
-*/
-app.service('$rsSession', ['$window',function ($window) {
-    this.set = function(key,value) {
-      $window.sessionStorage.setItem(key,value);
-    };
-    this.get = function(key) {
-      return $window.sessionStorage.getItem(key);
-    };
-    this.setUser = function(value) {
-      $window.sessionStorage.setItem('user',JSON.stringify(value));
-    };
-    this.getUser = function() {
-      return JSON.parse($window.sessionStorage.getItem('user'));
-    };
-    this.unset = function(key) {
-      $window.sessionStorage.removeItem(key);
-    };
-    this.remembered = function() {
-      return !!$window.sessionStorage.getItem('authToken');
-    };
-
-    this.setLocalAuth = function(authToken) {
-      $window.localStorage.setItem('authToken', authToken);
-    };
-    this.getLocalAuth = function() {
-      return $window.localStorage.getItem('authToken');
-    };
-
-    //Clears everything in local and session storage.
-    this.clear = function() {
-      $window.sessionStorage.clear();
-      $window.localStorage.clear();
-    };
-}]);
---
+++ angular/rs-auth-session-provider.js
@@ -0,0 +1,40 @@
+var app = angular.module('rs-auth');
+
+/**
+* Session Service
+* author: Jamie Spittal james@randomshapes.ca
+* Methods for getting and setting current session variables. Also, used for recalling a remembered session.
+*/
+app.service('$rsSession', ['$window',function ($window) {
+    this.set = function(key,value) {
+      $window.sessionStorage.setItem(key,value);
+    };
+    this.get = function(key) {
+      return $window.sessionStorage.getItem(key);
+    };
+    this.setUser = function(value) {
+      $window.sessionStorage.setItem('user',JSON.stringify(value));
+    };
+    this.getUser = function() {
+      return JSON.parse($window.sessionStorage.getItem('user'));
+    };
+    this.unset = function(key) {
+      $window.sessionStorage.removeItem(key);
+    };
+    this.remembered = function() {
+      return !!$window.sessionStorage.getItem('authToken');
+    };
+
+    this.setLocalAuth = function(authToken) {
+      $window.localStorage.setItem('authToken', authToken);
+    };
+    this.getLocalAuth = function() {
+      return $window.localStorage.getItem('authToken');
+    };
+
+    //Clears everything in local and session storage.
+    this.clear = function() {
+      $window.sessionStorage.clear();
+      $window.localStorage.clear();
+    };
+}]);
